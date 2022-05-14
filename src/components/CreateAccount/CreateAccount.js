29dfa3ad0bef1df3e35e8c0e03c2cd3ca7d9e1b7
SoubirSaian <soubir2018@gmail.com>
2022-05-14 20:42:12 +0600

    first-commit

---
+++ src/components/CreateAccount/CreateAccount.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CreateAccount.css';
+
+const CreateAccount = () => {
+    return (
+        <div>
+            <h3>MY DASH LLC</h3>
+        </div>
+    );
+};
+
+export default CreateAccount;
